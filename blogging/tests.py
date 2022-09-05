7810aa5248597e172c1628c8d5094b7a37a68773
Carl Parker <carljparker@fb.com>
2022-09-05 13:05:37 -0700

    Blacken the file

--- blogging/tests.py
+++ blogging/tests.py
@@ -9,20 +9,22 @@ from blogging.models import Category
 
 class FrontEndTestCase(TestCase):
     """test views provided in the front-end"""
-    fixtures = ['blogging_test_fixture.json', ]
+
+    fixtures = [
+        "blogging_test_fixture.json",
+    ]
 
     def setUp(self):
         self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
         self.timedelta = datetime.timedelta(15)
         author = User.objects.get(pk=1)
         for count in range(1, 11):
-            post = Post(title="Post %d Title" % count,
-                        text="foo",
-                        author=author)
+            post = Post(title="Post %d Title" % count, text="foo", author=author)
             if count < 6:
                 pubdate = self.now - self.timedelta * count
                 post.published_date = pubdate
             post.save()
+
 
 class PostTestCase(TestCase):
     fixtures = [
