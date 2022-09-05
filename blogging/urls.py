2f867ee7cdc48133353bc2928dd1410f0f950338
Carl Parker <carljparker@fb.com>
2022-09-05 14:34:36 -0700

    Blacken the file

--- blogging/urls.py
+++ blogging/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from blogging.views import stub_view, list_view
 
 urlpatterns = [
-    path('', list_view, name="blog_index"),
+    path("", list_view, name="blog_index"),
     path("posts/<int:post_id>/", stub_view, name="blog_detail"),
 ]
