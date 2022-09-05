2f478bd4a64b6316273bc9831dee7caafd4bbdaf
Carl Parker <carljparker@fb.com>
2022-09-04 22:13:00 -0700

    Add model and tests for categories

--- blogging/tests.py
+++ blogging/tests.py
@@ -14,3 +14,15 @@ class PostTestCase(TestCase):
         p1 = Post(title=expected)
         actual = str(p1)
         self.assertEqual(expected, actual)
+
+# another import
+from blogging.models import Category
+
+# and the test case and test
+class CategoryTestCase(TestCase):
+
+    def test_string_representation(self):
+        expected = "A Category"
+        c1 = Category(name=expected)
+        actual = str(c1)
+        self.assertEqual(expected, actual)
