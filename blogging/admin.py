fda18e6bb6bf03a11080d7b5a8dfaef01d77dd10
Carl Parker <carljparker@fb.com>
2022-09-07 17:08:45 -0700

    Working...apparently...need to play with it some more

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+#
 # Admin registrations
-admin.site.register(Post)
+#
 
-@admin.register(Category)
-class CategoryAdmin( admin.ModelAdmin ):
-    fields  = ( 'name', 'description' )
+class CategoriesInline(admin.TabularInline):
+    model = Category.posts.through
 
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    fields = ("name", "description")
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    inlines = [CategoriesInline]
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
