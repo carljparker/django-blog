2484dd32c9c31906091b07377ce083fccf49bfa8
Carl Parker <carljparker@fb.com>
2022-09-14 17:03:51 -0700

    Remove (already commented out) function-based views

--- blogging/views.py
+++ blogging/views.py
@@ -7,27 +7,11 @@ from django.views.generic.detail import DetailView
 
 # Create your views here.
 
-# def list_view(request):
-#     published = Post.objects.exclude(published_date__exact=None)
-#     posts = published.order_by("-published_date")
-#     context = {"posts": posts}
-#     return render(request, 'blogging/list.html', context)
-
-
 class PostListView(ListView):
     Model = Post
     queryset = Post.objects.order_by("-published_date")
     template_name = "blogging/list.html"
 
-
-# def detail_view(request, post_id):
-#     published = Post.objects.exclude(published_date__exact=None)
-#     try:
-#         post = published.get(pk=post_id)
-#     except Post.DoesNotExist:
-#         raise Http404
-#     context = {'post': post}
-#     return render(request, 'blogging/detail.html', context)
 
 class PostDetailView(DetailView):
     Model = Post
