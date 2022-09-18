d8596c3274d2c292a175a964532a07f1679a31b5
Carl Parker <carljparker@uw.edu>
2022-09-18 15:15:48 -0700

    Cody is correct: Don't need to specify the model in the class-based view...:-O

--- blogging/views.py
+++ blogging/views.py
@@ -8,12 +8,10 @@ from django.views.generic.detail import DetailView
 # Create your views here.
 
 class PostListView(ListView):
-    Model = Post
     queryset = Post.objects.exclude(published_date__exact=None).order_by("-published_date")
     template_name = "blogging/list.html"
 
 
 class PostDetailView(DetailView):
-    Model = Post
     queryset = Post.objects.exclude(published_date__exact=None)
     template_name = "blogging/detail.html"
